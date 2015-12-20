a83ef211711ecaf0b85776c4bf6d093ffa85d73d
Vuko <D13V03D2550>
2015-12-21 00:08:37 +0100

    - Prototyp skryptu Actor - Slidery hp actorow

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Actor : MonoBehaviour
+{
+    public int maxHealth;
+    public int health; //aktualne hp
+
+    private Slider healthBar;
+
+    public void setHpBar(Slider healthBar)
+    {
+        this.healthBar = healthBar;
+    }
+
+    public void Awake()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int damageValue)
+    {
+        if (health > 0)
+        {
+            health -= damageValue;
+        }
+        else
+        {
+            //todo: smierc
+            Debug.Log(name + " is dead");
+        }
+
+        if (healthBar != null)
+        {
+            healthBar.value = health;
+        }
+        else
+        {
+            Debug.Log("Actor.cs, slider problem");
+        }
+    }
+}
---
+++ Assets/Scripts/HealthBars.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBars : MonoBehaviour
+{
+    public static HealthBars instance = null;
+
+    public GameObject healthBarPrefab;
+    public GameObject healthBarParent;
+    public float healthBarYOffset;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        spawnSliders();
+    }
+
+    private void spawnSliders()
+    {
+        foreach (Transform child in transform)
+        {
+            Actor actor = child.GetComponent<Actor>();
+            if (actor != null)
+            {
+                spawnHitPointsSlider(actor);
+            }
+            else
+            {
+                Debug.Log(child.name + " :to nie jest aktor, czy na pewno umiesciles tu poprawny object?");
+            }
+        }
+    }
+
+    private void spawnHitPointsSlider(Actor actor)
+    {
+        GameObject healthObject = Instantiate(healthBarPrefab) as GameObject;
+        healthObject.transform.SetParent(healthBarParent.transform, false);
+
+        Slider healthBar = healthObject.GetComponentInChildren<Slider>();
+        actor.setHpBar(healthBar);
+
+        int maxHealth = actor.maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
+      
+        Vector3 worldPos = new Vector3(actor.transform.position.x, actor.transform.position.y + healthBarYOffset, actor.transform.position.z);
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(worldPos);
+        healthObject.transform.position = new Vector3(screenPos.x, screenPos.y, screenPos.z);
+    }
+}

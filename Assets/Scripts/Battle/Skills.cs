1bc7fe9f9b1c05f730e04c271dc0a9eff4b042c1
Piorunes <piorunes@gmail.com>
2016-01-03 19:48:22 +0100

    Dodanie buttona autoattack, zrealizowanie targetowania i działajacy raycast2d

---
+++ Assets/Scripts/Battle/Skills.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class Skill : MonoBehaviour
+{
+    public virtual void Action(Actor source) { } //Buffy i działające na ciebie skile
+    public virtual void Action(Actor source, Actor target) { } //
+   // public virtual void Action(Actor source, List<Actor> targets) { }
+}
---
+++ Assets/Scripts/Battle/Targetting.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Targetting : MonoBehaviour {
+
+    private bool isTargetting;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+       if (Input.GetMouseButtonDown(0) && isTargetting)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Vector2.zero, 10f);
+            Debug.Log("Position of click" + new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y));      
+            if (hit.collider != null  && hit.transform.tag == "Target")
+            {
+
+                Destroy(hit.collider.gameObject);
+                //Display the name of the parent of the object hit.
+                Debug.Log(hit.rigidbody.name);
+                isTargetting = false;
+            }
+        }
+
+    }
+
+    public void checkTarget()
+    {
+        isTargetting = true;
+    }
+}

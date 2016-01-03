a143d57db50d860d5ca51be61281a4e60cc2c035
Vuko <D13V03D2550>
2016-01-03 20:36:01 +0100

    - Prototyp poruszania sie po mapie - Graficzne generowanie pobliskich Tile

---
+++ Assets/Scripts/Exploring/Board.cs
@@ -0,0 +1,168 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Board : MonoBehaviour
+{
+    public static Board instance;
+
+    public Tile[,] board;
+    public int weight, height;
+    public Point currentTile;
+
+    public Image centerTile;
+    public Image upTile;
+    public Image downTile;
+    public Image rightTile;
+    public Image leftTile;
+
+    public Sprite emptyTile;
+    public Sprite blockTile;
+    public Sprite battleTile;
+
+    public struct Point
+    {
+        public int x;
+        public int y;
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+
+        generateBoard();
+        updateTiles();
+    }
+
+    public void updateTiles()
+    {
+        centerTile.sprite = getProperSprite(currentTile.x, currentTile.y);
+        upTile.sprite = getProperSprite(currentTile.x, currentTile.y + 1);
+        downTile.sprite = getProperSprite(currentTile.x, currentTile.y - 1);
+        rightTile.sprite = getProperSprite(currentTile.x + 1, currentTile.y);
+        leftTile.sprite = getProperSprite(currentTile.x - 1, currentTile.y);
+    }
+
+    private Sprite getProperSprite(int x, int y)
+    {
+        if (x < 0 || x >= weight || y < 0 || y >= height)
+        {
+            return emptyTile;
+        }
+
+        switch (board[x, y].type)
+        {
+            case Tile.Type.START: //todo: del
+                return blockTile;
+
+            case Tile.Type.END: //todo: del
+                return blockTile;
+
+            case Tile.Type.BATTLE:
+                return battleTile;
+
+            //case Tile.Type.BLOCK:
+            //    return blockTile;
+
+            default:
+                return emptyTile;
+        }
+        
+    }
+
+    private void generateBoard()
+    {
+        //todo: do zmiany all
+        board = new Tile[weight, height];
+        for (int i = 0; i < weight; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                board[i, j] = new Tile();
+            }
+        }
+
+        int count = weight * height;
+        int st = Random.Range(0, weight);
+        board[st, 0].type = Tile.Type.START;
+        currentTile.x = st;
+        currentTile.y = 0;
+        Debug.Log("start: " + currentTile.x + " " + currentTile.y);
+
+        int en = Random.Range(0, weight);
+        board[en, weight - 1].type = Tile.Type.END;
+
+        for (int i = 0; i <= 0.5 * count; i++)
+        {
+            int x = Random.Range(0, weight);
+            int y = Random.Range(0, height);
+
+            if (board[x, y].type == Tile.Type.EMPTY)
+            {
+                board[x, y].type = Tile.Type.BATTLE;
+            }
+        }
+
+        for (int i = height - 1; i >= 0; i--)
+        {
+            string str = "";
+            for (int j = 0; j < weight; j++)
+            {
+                str += board[j, i].type.ToString() + " ";
+            }
+            Debug.Log(str);
+        }
+    }
+
+    public class Tile
+    {
+        public Type type;
+        public enum Type
+        {
+            START,
+            END,
+            EMPTY,
+            BLOCK,
+            BATTLE //, EVENT?
+        }
+
+        public Tile()
+        {
+            type = Type.EMPTY;
+        }
+    }
+
+    public void move(int difX, int difY)
+    {
+        int newX = currentTile.x + difX;
+        int newY = currentTile.y + difY;
+        if (isMovePossible(newX, newY))
+        {
+            Debug.Log("nowa pozycja: " + newX + ", " + newY + " : " + board[newX, newY].type);
+            currentTile.x = newX;
+            currentTile.y = newY;
+        }
+    }
+
+    private bool isMovePossible(int x, int y)
+    {
+        if(x < 0 || x >= weight || y < 0 || y >= height)
+        {
+            Debug.Log("za krawedz wyszedl zes ;x");
+            return false;
+        }
+
+        Tile tile = board[x, y];
+        if(tile.type == Tile.Type.BLOCK)
+        {
+            Debug.Log("no na blocka to nie wejdziesz ziom");
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/Exploring/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	void Update ()
+    {
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            Board.instance.move(0, 1);
+            Board.instance.updateTiles();
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            Board.instance.move(0, -1);
+            Board.instance.updateTiles();
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            Board.instance.move(1, 0);
+            Board.instance.updateTiles();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            Board.instance.move(-1, 0);
+            Board.instance.updateTiles();
+        }
+    }
+}

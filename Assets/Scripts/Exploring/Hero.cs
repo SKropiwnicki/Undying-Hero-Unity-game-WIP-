751cf404df98e7c3e5be968e4f9bd7bdc9ea94f7
Vuko <D13V03D2550>
2016-01-04 01:04:09 +0100

    - Prototyp panelu dla Hero1 i Hero2

---
+++ Assets/Scripts/Exploring/Hero.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Hero : MonoBehaviour
+{
+    public int maxHealth;
+    public int health; //aktualne hp
+    public int initiative;
+
+    public int strength;
+    public int dexterity;
+    public int intelligence;
+    
+    public Text healthText;
+    public Text strengthText;
+    public Text dexterityText;
+    public Text intelligenceText;
+
+    void Awake()
+    {
+        setPanel();
+    }
+
+    private void setPanel()
+    {
+        healthText.text = "Health: " + health + "/" + maxHealth;
+        strengthText.text = "Strength: " + strength;
+        dexterityText.text = "Dexterity: " + dexterity;
+        intelligenceText.text = "Intelligence: " + intelligence;
+    }
+}

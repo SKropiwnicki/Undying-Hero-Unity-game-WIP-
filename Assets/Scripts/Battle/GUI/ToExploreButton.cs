59caeb6d3fe087dc6e325f4422d43bafbf0612d6
Vuko <D13V03D2550>
2016-01-06 02:23:56 +0100

    - Button z battle do fight działa...

---
+++ Assets/Scripts/Battle/GUI/ToExploreButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+using UnityEngine.SceneManagement;
+
+public class ToExploreButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("Exploring");
+    }
+}

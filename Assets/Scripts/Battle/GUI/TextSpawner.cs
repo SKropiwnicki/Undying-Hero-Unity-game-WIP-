07907a6035c98460dd5f75c875519eace31280d6
Vuko <D13V03D2550>
2016-01-13 21:23:04 +0100

    - TextSpawner - kolejki lokalne dla transformow

--- Assets/Scripts/Battle/GUI/TextSpawner.cs
+++ Assets/Scripts/Battle/GUI/TextSpawner.cs
@@ -8,7 +8,20 @@ public class TextSpawner : MonoBehaviour
 {
     public static TextSpawner instance = null;
 
-    private Queue<UnityAction> queue;
+    private List<tQueue> list;
+
+    public class tQueue
+    {
+        public Transform transform;
+        public Queue<UnityAction> queue;
+
+        public tQueue(Transform transform, UnityAction ua)
+        {
+            this.transform = transform;
+            queue = new Queue<UnityAction>();
+            queue.Enqueue(ua);
+        }
+    }
 
     public float timeBeforeNextSpawn = 0.5f;
 
@@ -22,15 +35,14 @@ public class TextSpawner : MonoBehaviour
 
     void Start()
     {
-        queue = new Queue<UnityAction>();
-        StartCoroutine("op");
+        list = new List<tQueue>();
     }
 
     public Text text;
     public GameObject textHolder;
     public float yOffset;
 
-    private IEnumerator op()
+    private IEnumerator op(Queue<UnityAction> queue)
     {
         while (!TurnManagement.instance.isBattleFinished)
         {
@@ -44,31 +56,40 @@ public class TextSpawner : MonoBehaviour
                 yield return new WaitForSeconds(timeBeforeNextSpawn);
             }
         }
+        yield return new WaitForSeconds(0.1f);
+    }
+
+    private void checkAndDo(Transform transform, UnityAction ua)
+    {
+        foreach (tQueue tq in list)
+        {
+            if (transform == tq.transform)
+            {
+                tq.queue.Enqueue(ua);
+                return;
+            }
+        }
+        tQueue tqx = new tQueue(transform, ua);
+        list.Add(tqx);
+        StartCoroutine(op(tqx.queue));
     }
 
     public void spawn(Transform transform, string str)
     {
-        UnityAction ua = () => { spawnx(transform, str); };
-        queue.Enqueue(ua);
+        UnityAction ua = () => { spawnx(transform, str, Color.red, 40); };
+        checkAndDo(transform, ua);
+    }
+
+    public void spawn(Transform transform, string str, Color color)
+    {
+        UnityAction ua = () => { spawnx(transform, str, color, 40); };
+        checkAndDo(transform, ua);
     }
 
     public void spawn(Transform transform, string str, Color color, int fontSize)
     {
         UnityAction ua = () => { spawnx(transform, str, color, fontSize); };
-        queue.Enqueue(ua);
-    }
-
-    private void spawnx(Transform transform, string str)
-    {
-        if (!transform) return;
-        text.text = str;
-
-        GameObject damageTextObject = Instantiate(text.gameObject) as GameObject;
-        damageTextObject.transform.SetParent(textHolder.transform, false);
-
-        Vector3 worldPos = new Vector3(transform.position.x, transform.position.y + yOffset, transform.position.z);
-        Vector3 screenPos = Camera.main.WorldToScreenPoint(worldPos);
-        damageTextObject.transform.position = new Vector3(screenPos.x, screenPos.y, screenPos.z);
+        checkAndDo(transform, ua);
     }
 
     private void spawnx(Transform transform, string str, Color color, int fontSize)

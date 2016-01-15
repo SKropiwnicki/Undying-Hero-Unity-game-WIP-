bbdee4493ff2dbbd6d14e449e3f3c5629105f95d
Vuko <D13V03D2550>
2016-01-15 22:49:59 +0100

    - Save profiles

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class Menu : MonoBehaviour
+{
+    public GameObject panel;
+    public static List<SaveLabel> sl;
+
+    void Start()
+    {
+        Debug.Log(Application.persistentDataPath);
+    }
+
+    public void playFunc()
+    {
+        sl = new List<SaveLabel>();
+        for(int i = 0; i < 3; i++)
+        {
+            sl.Add(new SaveLabel());
+        }
+
+        string path = Application.persistentDataPath;
+        BinaryFormatter bf = new BinaryFormatter();
+
+        for (int i = 1; i <= 3; i++)
+        {
+            if (!Directory.Exists(path + "/Player" + i) || !File.Exists(path + "/Player" + i + "/label.dat"))
+            {
+                Directory.CreateDirectory(path + "/Player" + i);
+                FileStream file = File.Create(path + "/Player" + i + "/label.dat");
+                bf.Serialize(file, sl[i-1]);
+                file.Close();
+            }
+            else
+            {
+                FileStream file = File.Open(Application.persistentDataPath + "/Player" + i + "/label.dat", FileMode.Open);
+                SaveLabel slx = (SaveLabel)bf.Deserialize(file);
+                file.Close();
+                sl[i - 1] = slx;
+            }
+        }
+
+        panel.SetActive(true);
+    }
+
+    public void quitFunc()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Menu/SaveSlot.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEditor;
+
+public class SaveSlot : MonoBehaviour
+{
+    public Button playButton;
+    public Button delButton;
+    public Button newGameButton;
+
+    public Text text;
+
+    public int count;
+
+    void Start()
+    {
+        setText();
+
+        UnityAction ua = new UnityAction(newGameFunc);
+        newGameButton.onClick.AddListener(ua);
+
+        ua = new UnityAction(delFunc);
+        delButton.onClick.AddListener(ua);
+
+        ua = new UnityAction(playFunc);
+        playButton.onClick.AddListener(ua);
+    }
+
+    private void setText()
+    {
+        int level = Menu.sl[count - 1].level;
+        if (level <= 0)
+        {
+            text.text = "Player " + count;
+            newGameButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            text.text = "Player " + count + "\n\nLevel: " + level;
+            playButton.gameObject.SetActive(true);
+            delButton.gameObject.SetActive(true);
+        }
+    }
+
+    public void newGameFunc()
+    {
+        playFunc();
+    }
+
+    public void delFunc()
+    {
+        FileUtil.DeleteFileOrDirectory(Application.persistentDataPath + "/Player" + count);
+        playButton.gameObject.SetActive(false);
+        delButton.gameObject.SetActive(false);
+        newGameButton.gameObject.SetActive(true);
+        text.text = "Player " + count;
+        newGameButton.gameObject.SetActive(true);
+    }
+
+    public void playFunc()
+    {
+        PlayerPrefs.SetInt("profileNumber", count);
+        SceneManager.LoadScene("START");
+    }
+}
